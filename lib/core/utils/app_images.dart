4b56a7faf8d99768c989d692abd051aa26c34a24
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-04 14:29:20 -0700

    use flutter assets Gen to generate image with extention

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  /// Assets for imagesSplashImage
+  /// assets/images/splash_image.jpg
+  static const String imagesSplashImage = "assets/images/splash_image.jpg";
+}
+
