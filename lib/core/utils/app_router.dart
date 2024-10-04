e5d9ad80fc02f98b3a28bc710a76509af1230c03
ZiadSala7 <zslah2800@gmail.com>
2024-10-04 15:00:23 +0300

    custom pass and text form field, login view has been created

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:doc_link_project/features/auth/login/presentation/views/login_view.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const LoginView(),
+      ),
+    ],
+  );
+}
