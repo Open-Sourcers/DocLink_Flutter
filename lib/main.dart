e0976eeb074cfa9917490ea571c1d3c9bc2ccb75
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-07 16:32:18 -0700

    add shared preferences database in class name  ( CacheHelper ) as private class then when you want use this class write CacheHelper.instant.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:doc_link_project/core/databases/cache/cache_helper.dart';
 import 'package:doc_link_project/doc_link_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  // this step used to set device not rotated
   WidgetsFlutterBinding.ensureInitialized();
+  // enable shared_preferences
+  CacheHelper.instant.init();
+  // this step used to set device not rotated
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
