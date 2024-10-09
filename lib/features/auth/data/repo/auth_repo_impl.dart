5254d356b06538d65ac70fb2c8555f9cbd247645
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-09 14:22:17 -0700

    refactor regiater and login in repo , send token to api and save token in local storage

--- lib/features/auth/data/repo/auth_repo_impl.dart
+++ lib/features/auth/data/repo/auth_repo_impl.dart
@@ -2,9 +2,13 @@ import 'package:dartz/dartz.dart';
 import 'package:doc_link_project/core/api/api_keys.dart';
 import 'package:doc_link_project/core/api/dio_consumer.dart';
 import 'package:doc_link_project/core/api/end_points.dart';
+import 'package:doc_link_project/core/databases/cache/cache_helper.dart';
 import 'package:doc_link_project/core/error/exception.dart';
+import 'package:doc_link_project/core/services/services_locator.dart';
 import 'package:doc_link_project/features/auth/data/models/auth_model/auth_model.dart';
 import 'package:doc_link_project/features/auth/data/repo/auth_repo.dart';
+import 'package:doc_link_project/generated/l10n.dart';
+import 'package:flutter/material.dart';
 
 class AuthRepoImpl implements AuthRepo {
   final DioConsumer dioConsumer;
@@ -12,7 +16,7 @@ class AuthRepoImpl implements AuthRepo {
   AuthRepoImpl({required this.dioConsumer});
 
   @override
-  Future<Either<String, AuthModel>> register(
+  Future<Either<String, AuthModel>> register(BuildContext context,
       {required String firstName,
       required String lastName,
       required String email,
@@ -26,16 +30,22 @@ class AuthRepoImpl implements AuthRepo {
       });
       AuthModel authModel = AuthModel.fromJson(response);
       if (authModel.statusCode == 200) {
+        cachedTokenInLocalStorage(authModel);
         return right(authModel);
       }
-      return left(authModel.message ?? "there was an error please try agsin");
+      return left(authModel.message ?? S.of(context).authError);
     } on ServerException catch (e) {
-      return left(e.errorModel.message ?? "there was an error please try agsin");
+      return left(
+          e.errorModel.message ?? S.of(context).authError);
     }
   }
 
+  void cachedTokenInLocalStorage(AuthModel authModel) {
+    getit<CacheHelper>().setString(ApiKeys.token, authModel.data!.token!);
+  }
+
   @override
-  Future<Either<String, AuthModel>> login(
+  Future<Either<String, AuthModel>> login(BuildContext context,
       {required String email, required String password}) async {
     try {
       final response = await dioConsumer.post(EndPoints.login, data: {
@@ -44,11 +54,13 @@ class AuthRepoImpl implements AuthRepo {
       });
       AuthModel authModel = AuthModel.fromJson(response);
       if (authModel.statusCode == 200) {
+        cachedTokenInLocalStorage(authModel);
         return right(authModel);
       }
-      return left(authModel.message ?? "there was an error please try agsin");
+      return left(authModel.message ?? S.of(context).authError);
     } on ServerException catch (e) {
-      return left(e.errorModel.message ?? "there was an error please try agsin");
+      return left(
+          e.errorModel.message ?? S.of(context).authError);
     }
   }
 }
