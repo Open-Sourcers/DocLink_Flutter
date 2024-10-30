d83ab5341fcff349069251a4a65194c93e48b6b4
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-30 16:18:05 -0700

    create about doctor section

---
+++ lib/features/user_views/doctor_details/presentation/view/widgets/about_doctor_section.dart
@@ -0,0 +1,25 @@
+import 'package:doc_link_project/core/utils/app_colors.dart';
+import 'package:doc_link_project/core/utils/styless.dart';
+import 'package:flutter/material.dart';
+
+class AboutDoctorSection extends StatelessWidget {
+  const AboutDoctorSection({super.key, required this.desc});
+  final String desc;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: Text(
+          "About Doctor",
+          style: Styless.textBold18.copyWith(color: AppColors.black),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: 12.0),
+          child: Text(desc, style: Styless.textRegular16),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/user_views/doctor_details/presentation/view/widgets/doctor_details_view_body.dart
+++ lib/features/user_views/doctor_details/presentation/view/widgets/doctor_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:doc_link_project/features/user_views/doctor_details/presentation/view/widgets/about_doctor_section.dart';
 import 'package:doc_link_project/features/user_views/doctor_details/presentation/view/widgets/doctor_details_header_section.dart';
 import 'package:doc_link_project/features/user_views/doctor_details/presentation/view/widgets/doctor_patients_and_experience_and_ratings.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,10 @@ class DoctorDetailsViewBody extends StatelessWidget {
               patientsNum: 1000,
               experienceYearsNum: 10,
               ratingsNum: 4.5,
+            ),
+            AboutDoctorSection(
+              desc:
+                  'Dr. Bellamy Nicholas is a top specialist at London Bridge Hospital at London. He has achieved several awards and recognition for is contribution and service in his own field. He is available for private consultation. ',
             ),
           ],
         ),
