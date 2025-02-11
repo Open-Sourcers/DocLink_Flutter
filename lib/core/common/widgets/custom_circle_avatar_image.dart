f714673b3183f6197d078c2774bc11469fcd7e97
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-11 07:29:12 -0800

    refactor custom cached network image provider

--- lib/core/common/widgets/custom_circle_avatar_image.dart
+++ lib/core/common/widgets/custom_circle_avatar_image.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 class CustomCircleAvatarImage extends StatelessWidget {
   final String? url;
   final String tempUrl =
-      'https://r.search.yahoo.com/_ylt=AwrEtYb5DE9nhkEzFd6JzbkF;_ylu=c2VjA3NyBHNsawNpbWcEb2lkAzYyOTNhMTZhNTA1NGM1YTkyNDI1ZTk3YTM2ODg3MjNhBGdwb3MDMTcEaXQDYmluZw--/RV=2/RE=1733262714/RO=11/RU=https%3a%2f%2fwww.fotolia.com%2fid%2f96549226/RK=2/RS=l_pAGbcEhhiNAF9fqeSjC6YfgXs-';
+      'https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg';
   const CustomCircleAvatarImage({
     super.key,
     required this.radius,
